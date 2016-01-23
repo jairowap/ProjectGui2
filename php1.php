51ccbe39a5427ebe173afa3bb03ed5773837105d
Jairo Rodriguez <jairowap@gmail.com>
2016-01-23 15:00:50 -0500

    Test Code PHP

---
+++ php1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php
+			$cedula = 94472;
+			$salario = 15; 
+			$nombre = "Jairo Rodriguez";
+			$activo = true;
+
+			$dia1=15;
+			$dia2=30;
+
+			echo "El numero de la cedula es: ";
+			echo $cedula;
+			echo "<br>";
+
+			echo "El trabajador devenga: ";
+			echo $salario;
+			echo "<br>";
+
+			echo "El nombre del empleado es: ";
+			echo $nombre;
+			echo "<br>";
+
+			echo "El empleado se encuentra activo: ";
+			echo $activo;
+			echo "<br>";
+			echo "<br>";	
+			
+			echo "El valor de la primer variable es:";
+			echo $dia1;
+			echo "<br>";
+			echo "El valor de la segunda variable es:";
+			echo $dia2;
+
+
+
+	?> 
+
+</body>
+</html>
+
+
